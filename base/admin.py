7797b7827a3ae613b94f31c315e5ddaec1b474bf
kuro0No <kurokun0908@gmail.com>
2022-08-28 00:04:33 +0700

    1

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Category, Prodcut
 # Register your models here.
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category,CategoryAdmin)
+
+
+class ProdcutAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price',]
+
+admin.site.register(Prodcut,ProdcutAdmin)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=True)
+
+class Prodcut(models.Model):
+    name = models.TextField(null=True)
+    price = models.FloatField()
+    # seller = models.ForeignKey(null=True, on_delete=models.CASCADE)
+    img = models.FileField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
