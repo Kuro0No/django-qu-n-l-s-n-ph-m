fae00b7df0adb579009e0477a5e0a0c2c1b963a6
kuro0No <kurokun0908@gmail.com>
2022-08-28 01:23:04 +0700

    123

--- base/views.py
+++ base/views.py
@@ -1,5 +1,14 @@
+from unicodedata import name
 from django.shortcuts import render
 # Create your views here.
 
+
+products = [
+    {'id': 1 , 'name': 'Hoodie', 'price': 50, 'seller': 'admin', 'category': 'Clothing' },
+    {'id': 2 , 'name': 'Hoodie', 'price': 100, 'seller': 'admin', 'category': 'Clothing' },
+    {'id': 3 , 'name': 'Hoodie', 'price': 150, 'seller': 'admin', 'category': 'Clothing' },
+]
+
 def home(request):
-    return render(request, 'index.html')
+    context = {'products': products}
+    return render(request, 'index.html', context)
